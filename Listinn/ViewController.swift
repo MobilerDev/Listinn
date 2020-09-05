a367476e06254b69a51b06f7e1d36daf9593dcc8
Yusa <yusasarisoy@icloud.com>
2020-09-06 01:29:14 +0300

    Initial Commit

---
+++ Listinn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Listinn
+//
+//  Created by Yuşa Sarısoy on 6.09.2020.
+//  Copyright © 2020 Yuşa Sarısoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
